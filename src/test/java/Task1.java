c8745f7c0f2420d205d00c2e9814273d7f1fe2a7
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-10-18 19:13:58 +0400

    Task1 on Task1 branch

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your score between 0-100");
+        int score = scanner.nextInt();
+
+        scanner.close();
+
+        if (score >=90 && score <=100){
+            System.out.println("Your grade is A");
+        }else if(score >=80 && score < 90){
+            System.out.println("Your grade is B");
+        } else if (score >=70 && score < 80) {
+            System.out.println("Your grade is C");
+        } else if (score >=60 && score < 70) {
+            System.out.println("Your grade is D");
+        }else {
+            System.out.println("Your grade is F");
+        }
+    }
 }
